b4c94fe6998dd7dab7a0cf35e07903b714ddae20
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-06-06 16:39:43 +0100

    socket logs message typed in box. Following socket.io docs for now.

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-let app = require('express')();
-let http = require('http').createServer(app);
+let app = require("express")();
+let http = require("http").createServer(app);
+let io = require("socket.io")(http);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// io.on("connection", (socket) => {
+//   console.log("a user connected");
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected");
+//   });
+// });
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
   });
+});
 
 http.listen(3000, () => {
-    console.log('listening on 3000.');
-})
+  console.log("listening on 3000.");
+});
